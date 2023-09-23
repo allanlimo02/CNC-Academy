4f12dfc2de1ae0e41a61d52c1b7572f0fd0bdb2e
allanlimo02 <allanlimo02@gmail.com>
2023-09-23 20:57:55 +0300

    Initial commit

---
+++ src/test/java/com/cnc_academy/CncAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnc_academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CncAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
