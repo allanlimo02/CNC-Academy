49f225ff48770a5831335b7f52ed83f6a88ff686
allanlimo02 <allanlimo02@gmail.com>
2023-09-23 22:29:05 +0300

    created student Interface

--- src/main/java/com/cnc_academy/students/Student.java
+++ src/main/java/com/cnc_academy/students/Student.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.Date;
 
